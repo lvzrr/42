64ea8128bd6da0d5bfd8758949db070a73871bcb
lvzrr <lvzrr@proton.me>
2025-03-03 03:54:14 +0100

    miau

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*   ft_is_prime.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/27 19:40:18 by jaicastr          #+#    #+#             */
-/*   Updated: 2025/02/27 19:42:21 by jaicastr         ###   ########.fr       */
+/*   Created: 2025/03/01 11:55:54 by jaicastr          #+#    #+#             */
+/*   Updated: 2025/03/01 11:59:28 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,31 @@ int	ft_sqrt(int nb)
 
 	if (nb < 0)
 		return (0);
-	if (nb == 0 || nb == 1)
-		return (nb);
-	init = 1;
-	while (init <= 46340 && init * init <= nb + 1)
+	init = 2;
+	while (init < 46341
+		|| init <= nb)
 	{
 		if (init * init == nb)
 			return (init);
 		init++;
 	}
 	return (0);
+}
+
+int	ft_is_prime(int nb)
+{
+	int	c;
+	int	sq;
+
+	c = 2;
+	sq = ft_sqrt(nb);
+	if (sq == 0)
+		sq = nb - 1;
+	while (c <= sq)
+	{
+		if (nb % c == 0)
+			return (0);
+		c++;
+	}
+	return (1);
 }
--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -1,29 +1,11 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sqrt.c                                          :+:      :+:    :+:   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/27 19:40:18 by jaicastr          #+#    #+#             */
-/*   Updated: 2025/02/27 19:42:21 by jaicastr         ###   ########.fr       */
+/*   Created: 2025/03/01 15:39:13 by jaicastr          #+#    #+#             */
+/*   Updated: 2025/03/01 16:16:37 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_sqrt(int nb)
-{
-	int	init;
-
-	if (nb < 0)
-		return (0);
-	if (nb == 0 || nb == 1)
-		return (nb);
-	init = 1;
-	while (init <= 46340 && init * init <= nb + 1)
-	{
-		if (init * init == nb)
-			return (init);
-		init++;
-	}
-	return (0);
-}
