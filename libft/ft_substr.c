744f7d5566d94e90b5e778a0e40c24327776ebb5
lvzrr <lvzrr@proton.me>
2025-04-08 14:10:38 +0200

    libft

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -20,12 +20,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	if (len == 0 || start >= ft_strlen(s))
 		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
 	out = malloc(len + 1);
 	if (!out)
 		return (NULL);
 	j = 0;
-	if (len > ft_strlen(s) - start)
-		len = ft_strlen(s) - start;
 	while (j < len && s[start])
 		out[j++] = s[start++];
 	out[j] = 0;
