52b6127728502d3736a53e280220d0c4d2a8a7b1
lvzrr <lvzrr@proton.me>
2025-04-08 18:23:50 +0200

    libft

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -5,53 +5,50 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaicastr <jaicastr@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/08 12:53:08 by jaicastr          #+#    #+#             */
-/*   Updated: 2025/04/08 13:05:45 by jaicastr         ###   ########.fr       */
+/*   Created: 2025/04/08 17:43:29 by jaicastr          #+#    #+#             */
+/*   Updated: 2025/04/08 17:44:23 by jaicastr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-static unsigned int	calc_size(long *_n)
+static unsigned int	count(int n)
 {
 	unsigned int	i;
-	long			tmp;
 
 	i = 0;
-	if (*_n <= 0)
-	{
-		*_n = -*_n;
+	if (n <= 0)
 		i++;
-	}
-	tmp = *_n;
-	while (*_n > 0)
+	while (n != 0)
 	{
 		i++;
-		*_n /= 10;
+		n /= 10;
 	}
-	*_n = tmp;
 	return (i);
+}
+
+static int	_abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	return (x);
 }
 
 char	*ft_itoa(int n)
 {
 	char			*out;
-	unsigned int	c;
-	long			_n;
+	unsigned int	cc;
 
-	_n = (long) n;
-	c = calc_size(&_n);
-	out = malloc(c + 1);
-	if (!out)
-		return (NULL);
+	cc = count(n);
+	out = malloc(cc + 1);
 	if (n < 0)
 		out[0] = '-';
-	out[c] = 0;
-	if (_n == 0)
-		out[0] = '0';
-	while (_n >= 0 && out[--c] != '-')
+	out[cc] = 0;
+	while (cc > 0 || n != 0)
 	{
-		out[c] = (_n % 10) + '0';
-		_n /= 10;
+		if (out[0] == '-' && cc == 1)
+			break ;
+		out[--cc] = '0' + _abs(n % 10);
+		n /= 10;
 	}
 	return (out);
 }
